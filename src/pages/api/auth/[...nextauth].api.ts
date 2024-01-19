2a8cae8ad234670a80edfc1490e2fab5fdcb6891
ROGERIO JOSE <57935315+Rogerio-17@users.noreply.github.com>
2024-01-18 22:22:05 -0300

    finalizando adapter

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,29 +1,45 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "../../../lib/auth/prisma-adapter"
+import { NextApiRequest, NextApiResponse } from "next"
 
-export const authOptions:NextAuthOptions = {
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse
+):NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
   
-  providers: [
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID ??'',
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    providers: [
+      GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID ??'',
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+  
+          authorization: {
+            params: {
+              scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar'
+            }
+          },
 
-        authorization: {
-          params: {
-            scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar'
+          profile(profile: GoogleProfiles){
+            profile
           }
+      }),
+    ],
+  
+    callbacks: {
+      async signIn({account}) {
+        if(!account?.scope?.includes('https://www.googleapis.com/auth/calendar')){
+          return '/register/connect-calendar/?error=permission'
         }
-    }),
-  ],
-
-  callbacks: {
-    async signIn({account}) {
-      if(!account?.scope?.includes('https://www.googleapis.com/auth/calendar')){
-        return '/register/connect-calendar/?error=permission'
+  
+        return true
       }
-
-      return true
     }
   }
 }
-export default NextAuth(authOptions)
+
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}
